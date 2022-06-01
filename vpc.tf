38320f26e9c7c29a91ca0f4d1afeb1dc2a827b83
selva-55 <81065499+selva-55@users.noreply.github.com>
2022-06-01 23:05:35 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {}
 
 resource "aws_vpc" "vpc" {
-    cidr_block = "172.16.0.0/24"
+    cidr_block = "172.16.0.0/16"
     enable_dns_support   = true
     enable_dns_hostnames = true
     tags       = {
