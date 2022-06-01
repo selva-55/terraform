40728bb5e047bd8976e14b1bfea92c0bfa488e1a
selva-55 <81065499+selva-55@users.noreply.github.com>
2022-06-01 23:06:04 +0530

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,10 @@
 resource "aws_subnet" "pub_subnet" {
     vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = "172.16.1.0/16"
+    cidr_block              = "172.16.1.0/24"
     availability_zone = "us-east-2a"
 }
 
 resource "aws_subnet" "pub2_subnet" {
     vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = "172.16.2.0/16"
+    cidr_block              = "172.16.2.0/24"
 }
